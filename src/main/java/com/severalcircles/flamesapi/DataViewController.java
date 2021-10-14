5e2df9f2123c8f017f6cf8515ebcea7c707b5628
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-10-14 11:18:23 -0400

    Initial commit

---
+++ src/main/java/com/severalcircles/flamesapi/DataViewController.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Several Circles.
+ */
+
+package com.severalcircles.flamesapi;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataViewController {
+
+}
